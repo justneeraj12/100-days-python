40d8a94bac957cef6134881da9acc5ed6ed2de69
Neeraj <140734621+justneeraj12@users.noreply.github.com>
2024-09-15 23:26:07 +0530

    Update Day 10.py

--- Day 10/Day 10.py
+++ Day 10/Day 10.py
@@ -17,3 +17,27 @@ operations = {
     "/": div,
 }
 
+
+def calculator():
+    should_accumulate = True
+    #print(operations["*"](4,8))
+    num1 = float(input("What is the first number?: "))
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+
+        operations_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the second number? "))
+        result = operations[operations_symbol](num1, num2)
+        print(f"{num1} {operations_symbol} {num2} = {result}")  
+
+        choice = input("type Y to continue with {result}, or type N to start new calculations.").lower()
+    
+
+        if choice == "y":
+            num1 = result
+        else: 
+            should_accumulate = False
+            print("\n"*20)
+            calculator()
+calculator()
